71821d89f1655aa88a1b58cedeed43f8bb0144f3
devrim <56955202+Devrim0@users.noreply.github.com>
2022-06-15 20:48:48 +0300

    ödev hazır

--- chalange.java
+++ chalange.java
@@ -1,11 +1,13 @@
 public class AA{
     
     public static int dizi(int dizi[]){
+        
        return 0;
     }
-    public static void method(int dizi[]){
+    public static int[] method(int dizi[]){
         dizi[0]=0;
         dizi[1]=dizi;
+        return dizi;
     }
 
    public static void main(String[] args) {
